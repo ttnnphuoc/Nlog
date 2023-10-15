aabd460b58d187a7c9c71d652f05ea137dd3f36e
Tran, Phuoc (Jon) <60534054+ttnnphuoc@users.noreply.github.com>
2023-10-15 17:12:21 +0700

    Logging in ASP .NET Core With NLog

---
+++ ImplementNLog/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ImplementNLog.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "Logs Added in Controller");
+
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("Hello, I have added logs");
+        }
+    }
+}
